57ae6f52d3f0896941d4a4906e74ce94e8bb0a7b
jrfcabral <joaofigueiredocabral@gmail.com>
2016-04-04 15:17:48 +0100

    domain specifications

--- db/PDM.sql
+++ db/PDM.sql
@@ -1,4 +1,6 @@
-CREATE TABLE User(
+CREATE TYPE ROLE AS ENUM ('COORD', 'MEMBER');
+ 
+CREATE TABLE USER(
 	userID INT SERIAL PRIMARY KEY NOT NULL,
 	username VARCHAR(25) NOT NULL UNIQUE,
 	password VARCHAR(512) NOT NULL,
@@ -9,83 +11,85 @@ CREATE TABLE User(
 	education VARCHAR(25),
 	CONSTRAINT joinBeforeLogout CHECK (lastLogout > joinDate)
 );
-
+ 
 CREATE TABLE Project(
 	projectID INT SERIAL PRIMARY KEY NOT NULL,
 	name VARCHAR(25) UNIQUE NOT NULL,
-	creator INT REFERENCES User(userID),
+	creator INT REFERENCES USER(userID),
 	creationDate TIMESTAMP NOT NULL,
 	CONSTRAINT joinBeforeCreating CHECK (creationDate > creator.joinDate)
 );
-
-CREATE TABLE Role(
-	userID INT REFERENCES User(userID) NOT NULL,
+ 
+CREATE TABLE ROLE(
+	userID INT REFERENCES USER(userID) NOT NULL,
 	projectID INT REFERENCES Project(projectID) NOT NULL,
-	role VARCHAR(12) NOT NULL,
-	CONSTRAINT chk_role CHECK (role IN ('COORD', 'MEMBER')),
+	ROLE ROLE NOT NULL,
+	CONSTRAINT chk_role CHECK (ROLE IN ('COORD', 'MEMBER')),
 	PRIMARY KEY(userID, projectID)
 );
-
+ 
 CREATE TABLE TaskList(
 	taskLiID INT SERIAL PRIMARY KEY NOT NULL,
 	name VARCAHR(25) NOT NULL
 );
-
-
+ 
+ 
 CREATE TABLE Task(
 	taskID INT SERIAL PRIMARY KEY NOT NULL,
 	projectID INT REFERENCES Project(projectID) NOT NULL,
-	creator INT REFERENCES User(userID) NOT NULL,
-	assignee INT REFERENCES User(userID),
+	creator INT REFERENCES USER(userID) NOT NULL,
+	assignee INT REFERENCES USER(userID),
 	name VARCHAR(25) NOT NULL,
 	taskLiID INT REFERENCES TaskList(taskLiID),
 	creationInfo TIMESTAMP,
-	CONSTRAINT createBeforeTask CHECK (creationInfo > projectID.creationDate)
+	CONSTRAINT createBeforeTask CHECK (creationInfo > projectID.creationDate),
+        UNIQUE(projectID, name)
 );
-
+ 
 CREATE TABLE TaskLabel(
 	taskLID INT SERIAL PRIMARY KEY NOT NULL,
 	name VARCHAR(15)
 );
-
+ 
 CREATE TABLE TaskToLabel(
 	taskID INT REFERENCES Task(taskID) NOT NULL,
 	taskLID INT REFERENCES TaskLabel(taskLID) NOT NULL,
 	PRIMARY KEY(taskID, taskLID)
 );
-
+ 
 CREATE TABLE TaskComment(
 	taskCID INT SERIAL PRIMARY KEY NOT NULL,
 	taskID INT REFERENCES Task(taskID) NOT NULL,
-	commentor INT REFERENCES User(userID) NOT NULL,
+	commenter INT REFERENCES USER(userID) NOT NULL,
 	creationInfo TIMESTAMP NOT NULL,
 	text VARCHAR(512) NOT NULL,
 	CONSTRAINT createBeforeComment CHECK (creationInfo > taskID.creationInfo)
 );
-
+ 
 CREATE TABLE Thread(
 	threadID INT SERIAL PRIMARY KEY NOT NULL,
 	projectID INT REFERENCES Project(projectID) NOT NULL,
-	creator INT REFERENCES User(userID) NOT NULL,
+	creator INT REFERENCES USER(userID) NOT NULL,
 	name VARCHAR(25) NOT NULL,
 	creationInfo TIEMSTAMP NOT NULL,
-	CONSTRAINT createBeforeThread CHECK (creationInfo > projectID.creationDate)
+	CONSTRAINT createBeforeThread CHECK (creationInfo > projectID.creationDate),
+        UNIQUE(projectID, name)
 );
-
+ 
 CREATE TABLE Comment(
 	commentID INT SERIAL PRIMARY KEY NOT NULL,
 	threadID INT REFERENCES Thread(threadID) NOT NULL,
-	commentor INT REFERENCES User(userID) NOT NULL,
+	commenter INT REFERENCES USER(userID) NOT NULL,
 	creationInfo TIMESTAMP NOT NULL,
 	text VARCHAR(512) NOT NULL,
 	CONSTRAINT createBeforeComment CHECK (creationInfo > threadID.creationInfo)
 );
-
+ 
 CREATE TABLE ThreadLabel(
 	threadLID INT SERIAL PRIMARY KEY NOT NULL,
 	name VARCHAR(15) NOT NULL
 );
-
+ 
 CREATE TABLE ThreadToLabel(
 	threadID INT REFERENCES Thread(threadID),
 	threadLID INT REFERENCES ThreadLabel(threadLID),
