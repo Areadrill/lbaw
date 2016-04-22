739b4271fe7d9260a277dfdc9dc2bd0b92c7f20b
jrfcabral <joaofigueiredocabral@gmail.com>
2016-04-22 14:08:56 +0100

    make config great again

--- proto/config/init.php
+++ proto/config/init.php
@@ -1,19 +1,20 @@
 <?php
-  session_set_cookie_params(3600, '/~joao');
+  session_set_cookie_params(3600, '/~lbaw1536');
   session_start();
 
-  error_reporting(E_ALL); // E_NOTICE by default
-  ini_set("display_errors", "1");
-  $BASE_DIR = '/home/joao/public_html/lbaw/proto/';
-  $BASE_URL = '/~joao/lbaw/proto/';
+  error_reporting(E_ERROR | E_WARNING); // E_NOTICE by default
 
-  $conn = new PDO('pgsql:host=localhost;dbname=lbaw', 'postgres', 'bolachas');
+  $BASE_DIR = '/opt/lbaw/lbaw1536/public_html/proto/';
+  $BASE_URL = '/~lbaw1536/proto/';
+
+  $conn = new PDO('pgsql:host=dbm;dbname=lbaw1536', 'lbaw1536', 'LS920UL3');
   $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   $conn->exec('SET SCHEMA \'proto\'');
 
   include_once($BASE_DIR . 'lib/smarty/Smarty.class.php');
+
   $smarty = new Smarty;
   $smarty->template_dir = $BASE_DIR . 'templates/';
   $smarty->compile_dir = $BASE_DIR . 'templates_c/';
