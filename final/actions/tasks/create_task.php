ecd5c515127e3b8b509d10972618271b038c06dd
jrfcabral <joaofigueiredocabral@gmail.com>
2016-06-07 09:46:29 +0100

    fix creation bug

--- final/actions/tasks/create_task.php
+++ final/actions/tasks/create_task.php
@@ -12,9 +12,9 @@ if (!$_POST['projectid'] || !$_POST['name'] || !$_POST['body']) {
 $title = strip_tags($_POST['name']);
 $initComment = strip_tags($_POST['body']);
 
-if(strlen($title) > 25){
+if(strlen($title) > 50){
 	$_SESSION['error_messages'][] = "Too many characters in one of the form's fields";
-	header("Location: ". $BASE_URL . 'pages/projectpage.php?id='.$_POST['projid']);
+	header('Location: ' . $_SERVER['HTTP_REFERER']);
 	exit;
 }
 
@@ -24,8 +24,11 @@ if (!isset($_SESSION['userid'])){
 	header('Location: ' . $_SERVER['HTTP_REFERER']);
 	exit;
 }
+var_dump("ok");
 try{
 $res = createTask($_POST['name'], $_POST['projectid'], $_SESSION['userid'], $_POST['body']);
+var_dump($res);
+exit;
 }
 catch(Exception $e){
 	$_SESSION['error_messages'][] = 'Duplicated task name not allowed';
